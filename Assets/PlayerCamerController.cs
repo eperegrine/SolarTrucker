ecf8b429762dd109584a87bd6c176c65b85e8748
eperegrine <peregrineemily@gmail.com>
2023-09-30 19:07:09 +0100

    Made ship fly

--- Assets/PlayerCamerController.cs
+++ Assets/PlayerCamerController.cs
@@ -1,18 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D Target;
+    public Camera cam;
+
+    public float MinSize = 7f;
+    public float MaxSize = 40f;
+    
+    private void Start()
     {
-        
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Target.velocity.magnitude);
+        var newSize = Target.velocity.magnitude;
+        newSize = Mathf.Max(MinSize, newSize);
+        newSize = Mathf.Min(MaxSize, newSize);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newSize, .1f);
     }
 }
